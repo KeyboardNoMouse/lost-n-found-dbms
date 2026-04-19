9e64937deeb9c10ac5740d51297efacded857780
Shreedhar Hegade <shreedharhegade.18@gmail.com>
2026-04-19 16:51:50 +0530

    fix: added domain-based authentication

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,9 +6,23 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+          hd: "nie.ac.in",
+        }
+      }
     }),
   ],
   callbacks: {
+    async signIn({ account, profile }) {
+      if (account?.provider === "google") {
+        return profile?.email?.endsWith("@nie.ac.in") ?? false;
+      }
+      return true;
+    },
     async session({ session, token }) {
       return session;
     },
